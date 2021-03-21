13be6aa99d7c69fca1abcf479c6ea5d1689b6ecf
kraj0004 <kraj0004@student.monash.edu>
2021-03-22 00:31:02 +1100

    Second

--- practice/src/practice/classjhg.java
+++ practice/src/practice/classjhg.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class classjhg {
-
-}
+ class uj {
+	public  int sum(int k) {
+	    if (k > 0) {
+	      return k + sum(k - 1);
+	    } else {
+	      return 0;
+	    }
+	}
+ }
+	    class classjhg
+	    
+	    {
+	    	
+		 public static void main(String[] args) 
+		 {
+		 
+			 uj u = new uj();
+			 
+			 int result = u.sum(10);
+		    System.out.println(result);
+		  }
+		  
+		  }
+	 
